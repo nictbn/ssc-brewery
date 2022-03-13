62221edca02e030c41a9ba46b5e533289142a9d9
Nicu Taban <nictbndeveloper@gmail.com>
2022-03-13 17:19:58 +0200

    Creates header authentication filter

--- src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -3,6 +3,8 @@ package guru.sfg.brewery.web.controllers.api;
 import guru.sfg.brewery.web.controllers.BaseIT;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -23,6 +25,14 @@ public class BeerRestControllerIT extends BaseIT {
     @Test
     void findBeerByUpc() throws Exception {
         mockMvc.perform(get("/api/v1/beerUpc/0631234200036"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteBeer() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/87d2da00-b715-4dd0-a8fd-8d762976e168")
+                .header("Api-Key", "spring")
+                .header("Api-Secret", "guru"))
                 .andExpect(status().isOk());
     }
 }
