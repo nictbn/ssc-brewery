48e0e04c372380ab764420a7aba49295d3cc814e
Nicu Taban <nictbndeveloper@gmail.com>
2022-03-19 16:17:40 +0200

    Enables method security

--- src/test/java/guru/sfg/brewery/web/controllers/BaseIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BaseIT.java
@@ -32,6 +32,11 @@ public abstract class BaseIT {
                 Arguments.of("user", "password"));
     }
 
+    public static Stream<Arguments> getStreamAdminCustomer() {
+        return Stream.of(Arguments.of("spring" , "guru"),
+                Arguments.of("scott", "tiger"));
+    }
+
     public static Stream<Arguments> getStreamNotAdmin() {
         return Stream.of(Arguments.of("scott", "tiger"),
                 Arguments.of("user", "password"));
