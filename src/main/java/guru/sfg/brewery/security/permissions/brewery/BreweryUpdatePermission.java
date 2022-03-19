182b1df3f39f9f3ec5945fd3f5e8935204fe8a5a
Nicu Taban <nictbndeveloper@gmail.com>
2022-03-19 21:06:24 +0200

    Update all controllers to use annotations

--- src/main/java/guru/sfg/brewery/security/permissions/brewery/BreweryUpdatePermission.java
+++ src/main/java/guru/sfg/brewery/security/permissions/brewery/BreweryUpdatePermission.java
@@ -1,4 +1,4 @@
-package guru.sfg.brewery.security.perms;
+package guru.sfg.brewery.security.permissions.brewery;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 
@@ -6,6 +6,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
-@PreAuthorize("hasAuthority('beer.create')")
-public @interface BeerCreatePermission {
+@PreAuthorize("hasAuthority('brewery.update')")
+public @interface BreweryUpdatePermission {
 }
