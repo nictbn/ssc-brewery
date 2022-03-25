4a4bdc7045082fc92ea4f94bd08a3d694cece120
Nicu Taban <nictbndeveloper@gmail.com>
2022-03-25 23:08:47 +0200

    Configures Successful Log InEvent Publisher and Listener

--- src/main/java/guru/sfg/brewery/config/SecurityBeans.java
+++ src/main/java/guru/sfg/brewery/config/SecurityBeans.java
@@ -1,7 +1,10 @@
 package guru.sfg.brewery.config;
 
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
+import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
@@ -14,5 +17,10 @@ public class SecurityBeans {
         JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
         tokenRepository.setDataSource(dataSource);
         return tokenRepository;
+    }
+
+    @Bean
+    public AuthenticationEventPublisher authenticationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        return new DefaultAuthenticationEventPublisher();
     }
 }
