3cbda87aebe3aa5adafa6a2dd08efc64a72e4e86
Nicu Taban <nictbndeveloper@gmail.com>
2022-03-26 17:06:00 +0200

    Configures Spring MVC CORS Configuration

--- src/main/java/guru/sfg/brewery/config/WebConfig.java
+++ src/main/java/guru/sfg/brewery/config/WebConfig.java
@@ -6,8 +6,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT");
+//    }
 }
