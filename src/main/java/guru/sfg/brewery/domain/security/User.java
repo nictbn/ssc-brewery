0ced9efad3aa2ff607865fe41f81b4a599784a0c
Nicu Taban <nictbndeveloper@gmail.com>
2022-03-26 14:07:39 +0200

    Adds automatic unlock of accounts

--- src/main/java/guru/sfg/brewery/domain/security/User.java
+++ src/main/java/guru/sfg/brewery/domain/security/User.java
@@ -2,12 +2,15 @@ package guru.sfg.brewery.domain.security;
 
 import guru.sfg.brewery.domain.Customer;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.CredentialsContainer;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -52,6 +55,13 @@ public class User implements UserDetails, CredentialsContainer {
     private Boolean credentialsNonExpired = true;
     @Builder.Default
     private Boolean enabled = true;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
 
     @ManyToOne(fetch = FetchType.EAGER)
     private Customer customer;
